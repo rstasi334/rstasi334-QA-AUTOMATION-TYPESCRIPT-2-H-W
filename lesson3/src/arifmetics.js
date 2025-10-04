0c1a5f2fb5d87bdabb9116b6e1d8adf371073dd1
Stanislav Rodzin <stanislaw.rodzin@gmail.com>
2025-10-04 21:51:09 +0200

    added lesson3 home work

---
+++ lesson3/src/arifmetics.js
@@ -0,0 +1,12 @@
+function NumbersAndStrings() {
+    const a = Number('5');
+    const b = Number('10');
+    const c = String(20);
+    const d = String(30);
+    const e = String('text');
+
+    console.log(a + b, b - a, a + c, d + c, a * b, d / a, a + e);
+
+}
+
+NumbersAndStrings();
